28532d907106f5760fe13000ccde294e99862410
Vasiliy Tekhin <fartoviy109@gmail.com>
2021-06-06 20:33:42 +0300

    v 1.2 AllTestPassed

--- CountPrimes/main.py
+++ CountPrimes/main.py
@@ -1,12 +1,13 @@
 class CountPrimes:
     def test_count_primes(self, function):
-        list_of_numbers = [0, 1, 2, 5, 10, -1, ]
-        number_of_correct_numbers = [0, 0, 1, 3, 4, 'Assert_Error', ]
+        list_of_numbers = [0, 1, 2, 3, 5, 10, -1, ]
+        number_of_correct_numbers = [0, 0, 0, 1, 2, 4, 'Assert_Error', ]
         for i, number in enumerate(list_of_numbers):
             if function(number) == number_of_correct_numbers[i]:
                 print('Test{0} ---- Passed'.format(i))
             else:
                 print('Test{0} ---- Failed'.format(i))
+                print(function(number))
 
     def countprimes(self, n: int) -> int:
         """Program have to return quantity of prime numbers,
@@ -14,14 +15,23 @@ class CountPrimes:
         0 <= n <= 5 * 10^6.
         """
         assert n >= 0, 'n must be greater than 0'
-        counter = 0
-        if n < 2:
+        counter = [2, ]
+        if n <= 2:
             return 0
-        elif n == 2:
+        elif n == 3:
             return 1
         else:
-            for i in range(3, n+1, 2):
-                pass
+            for number in range(3, n, 2):
+                flag = True
+                for primes in counter:
+                    if primes > int(number**0.5) + 1:
+                        break
+                    elif number % primes == 0:
+                        flag = False
+                        break
+                if flag:
+                    counter.append(number)
+            return len(counter)
 
 
 test = CountPrimes()
