f6ab425bbb76d507771504a37f76bd0b4c7a46b0
Vasiliy Tekhin <fartoviy109@gmail.com>
2021-06-06 19:16:44 +0300

    v 1.1 MaDeTestFunction

--- CountPrimes/main.py
+++ CountPrimes/main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+class CountPrimes:
+    def test_count_primes(self, function):
+        list_of_numbers = [0, 1, 2, 5, 10, -1, ]
+        number_of_correct_numbers = [0, 0, 1, 3, 4, 'Assert_Error', ]
+        for i, number in enumerate(list_of_numbers):
+            if function(number) == number_of_correct_numbers[i]:
+                print('Test{0} ---- Passed'.format(i))
+            else:
+                print('Test{0} ---- Failed'.format(i))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def countprimes(self, n: int) -> int:
+        """Program have to return quantity of prime numbers,
+        less than n.
+        0 <= n <= 5 * 10^6.
+        """
+        assert n >= 0, 'n must be greater than 0'
+        counter = 0
+        if n < 2:
+            return 0
+        elif n == 2:
+            return 1
+        else:
+            for i in range(3, n+1, 2):
+                pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+test = CountPrimes()
+test.test_count_primes(test.countprimes)
