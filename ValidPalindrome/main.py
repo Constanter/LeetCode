b97f2a74fb3021efb593fb23c76e19e8f9d1cd74
Vasiliy Tekhin <fartoviy109@gmail.com>
2021-06-07 07:31:45 +0300

    v 1.1 All test passed,made two version of function

--- ValidPalindrome/main.py
+++ ValidPalindrome/main.py
@@ -1,10 +1,37 @@
+import re
+
+
 class ValidPalindrome:
     def test_valid_palindrome(self, function):
-        pass
+        test_cases = ["A man, a plan, a canal: Panama", "race a car", '', ]
+        right_answers = [True, False, True, ]
+        for i, answer in enumerate(test_cases):
+            if function(answer) == right_answers[i]:
+                print("Test{0} --- Passed".format(i))
+            else:
+                print("Test{0} --- Failed".format(i))
 
     def valid_palindrom(self, s: str) -> bool:
-        pass
+        """Get a string, and return True if string is palindrome,
+        or return False if is not.We are ignoring cases, and non alphanumeric symbols
+        """
+        result_string = "".join([x for x in s if x.isalnum()]).lower()
+        return result_string == result_string[::-1]
+
+    def optimal_valid_palindrom(self, s: str) -> bool:
+        s1 = re.sub("[^0-9a-zA-Z]+", "", s).lower()
+
+        start, end = 0, len(s1) - 1
+
+        while start < end:
+            if s1[start] != s1[end]:
+                return False
+            start += 1
+            end -= 1
+
+        return True
 
 
 test = ValidPalindrome()
 test.test_valid_palindrome(test.valid_palindrom)
+test.test_valid_palindrome(test.optimal_valid_palindrom)
