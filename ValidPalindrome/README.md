17cefbe5db59e97d950fc7a56baee72988dd772f
Vasiliy Tekhin <fartoviy109@gmail.com>
2021-06-07 07:08:54 +0300

    v 1.0

---
+++ ValidPalindrome/README.md
@@ -0,0 +1,3 @@
+#   Valid Palindrome
+Given a string s, determine if it is a palindrome, 
+considering only alphanumeric characters and ignoring cases.
---
+++ ValidPalindrome/main.py
@@ -0,0 +1,10 @@
+class ValidPalindrome:
+    def test_valid_palindrome(self, function):
+        pass
+
+    def valid_palindrom(self, s: str) -> bool:
+        pass
+
+
+test = ValidPalindrome()
+test.test_valid_palindrome(test.valid_palindrom)
