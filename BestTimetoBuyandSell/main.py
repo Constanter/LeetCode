5086b1861a1e450211ecb2303165689c6240855b
Vasiliy Tekhin <fartoviy109@gmail.com>
2021-06-08 06:58:35 +0300

    v 1.0 initial commit

---
+++ BestTimetoBuyandSell/main.py
@@ -0,0 +1,9 @@
+class BestBuyAndSell:
+    def test_max_profit(self, function):
+        pass
+
+    def maxProfit(self, prices: list[int]) -> int:
+        pass
+
+test = BestBuyAndSell()
+test.test_max_profit(test.maxProfit)
