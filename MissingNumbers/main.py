9c309e89aa5157e1dd72cfa609835c2952805190
Vasiliy Tekhin <fartoviy109@gmail.com>
2021-06-09 06:55:14 +0300

    initial commit.Made Readme and skeleton of class Solution

---
+++ MissingNumbers/main.py
@@ -0,0 +1,10 @@
+class MissingNumbers:
+    def test_missingnumber(self, functiion):
+        pass
+
+    def missingnumber(self, nums: list[int]) -> int:
+        pass
+
+
+test =MissingNumbers()
+test.test_missingnumber(test.missingnumber)
