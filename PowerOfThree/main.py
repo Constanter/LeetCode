66636c0091a6f1d342be6894bc3b1ccd6b59cfbb
Vasiliy Tekhin <fartoviy109@gmail.com>
2021-06-10 07:07:43 +0300

    all test passed

--- PowerOfThree/main.py
+++ PowerOfThree/main.py
@@ -1,10 +1,22 @@
 class PowersOfThree:
-    def power_of_three(self, n : int) -> bool:
-        pass
+    def isPowerOfThree(self, n: int) -> bool:
+        counter = 1
+        while True:
+            if counter > n:
+                return False
+            elif counter == n:
+                return True
+            else:
+                counter *= 3
 
-    def test_power_of_three(self, fuction):
-        pass
-
+    def test_power_of_three(self, function):
+        test_cases = [9, 0, 45, 1, 77, 81, ]
+        right_aswers = [True, False, False, True, False, True, ]
+        for i, case in enumerate(test_cases):
+            if function(case) == right_aswers[i]:
+                print("Test{0} --- Passed")
+            else:
+                print("Test{0} --- Failed")
 
 test = PowersOfThree()
-test.test_power_of_three(test.power_of_three)
+test.test_power_of_three(test.isPowerOfThree)
