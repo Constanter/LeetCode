f7ca80c4b0bae6cad1d6aacdcb343265606776cf
Vasiliy Tekhin <fartoviy109@gmail.com>
2021-06-10 07:02:34 +0300

    initial commit

---
+++ PowerOfThree/main.py
@@ -0,0 +1,10 @@
+class PowersOfThree:
+    def power_of_three(self, n : int) -> bool:
+        pass
+
+    def test_power_of_three(self, fuction):
+        pass
+
+
+test = PowersOfThree()
+test.test_power_of_three(test.power_of_three)
